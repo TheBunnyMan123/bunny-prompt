f56dff1996c7db54886ac1873f0378c9f2a7da1f
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-09 16:09:33 -0600

    Add more git tracking, and first check if git is installed

--- Program.cs
+++ Program.cs
@@ -17,9 +17,15 @@ class bunnyPrompt {
   private static bool trueColor = false;
   private static string workDir = Directory.GetCurrentDirectory();
   private static string branch = "";
+  private static int unstagedChanges = 0;
+  private static int stagedChanges = 0;
 
   private const string gitIcon = "\ue65d ";
   private const string gitBranchIcon = "\uf418 ";
+  private const string gitUnstagedIcon = "\uf040 ";
+  private const string gitStagedIcon = "\uebbc ";
+
+  private static bool isGit = false;
 
   private static string ps1 = "\r\n";
 
@@ -28,27 +34,79 @@ class bunnyPrompt {
     if (colorTerm == null) {colorTerm = "";}
     if ((colorTerm.ToLower() == "truecolor") || (colorTerm.ToLower() == "24bit")) {trueColor = true;}
 
-    if (trueColor == false) {ps1 = ps1 + @"\[\x1b[1m\]";}
+    if (trueColor == false) {ps1 = ps1 + @"\[\033[1m\]";}
 
-    ProcessStartInfo gitStartInfo = new ProcessStartInfo("git");
+    ProcessStartInfo gitStartInfo1 = new ProcessStartInfo("bash", "-c \"command -v git\"");
+    gitStartInfo1.RedirectStandardOutput = true;
 
-    gitStartInfo.UseShellExecute = false;
-    gitStartInfo.WorkingDirectory = workDir;
-    gitStartInfo.RedirectStandardInput = true;
-    gitStartInfo.RedirectStandardOutput = true;
-    gitStartInfo.Arguments = "rev-parse --is-inside-work-tree";
+    Process gitProcess1 = Process.Start(gitStartInfo1);
+    gitProcess1.WaitForExit();
 
-    Process gitProcess = new Process();
-    gitProcess.StartInfo = gitStartInfo;
-    gitProcess.Start();
+    int exitCode = gitProcess1.ExitCode;
 
-    bool isGit = false;
-    Boolean.TryParse(gitProcess.StandardOutput.ReadLine(), out isGit);
+    if (exitCode == 0)
+    {
+      ProcessStartInfo gitStartInfo = new ProcessStartInfo("git");
 
-    gitProcess.Dispose(); 
+      gitStartInfo.UseShellExecute = false;
+      gitStartInfo.WorkingDirectory = workDir;
+      gitStartInfo.RedirectStandardInput = true;
+      gitStartInfo.RedirectStandardOutput = true;
+      gitStartInfo.Arguments = "rev-parse --is-inside-work-tree";
+
+      Process gitProcess = new Process();
+      gitProcess.StartInfo = gitStartInfo;
+      gitProcess.Start();
+
+      Boolean.TryParse(gitProcess.StandardOutput.ReadLine(), out isGit);
+
+      gitProcess.Dispose(); 
+    }
 
     if (isGit) {
       displayTable.Add(Display.GitBranch);
+
+      ProcessStartInfo startInfo = new ProcessStartInfo("git");
+
+      startInfo.UseShellExecute = false;
+      startInfo.WorkingDirectory = workDir;
+      startInfo.RedirectStandardInput = true;
+      startInfo.RedirectStandardOutput = true;
+      startInfo.Arguments = "diff --name-status";
+
+      Process process = new Process();
+      process.StartInfo = startInfo;
+      process.Start();
+
+      // Wait for the process to finish
+      process.WaitForExit();
+
+      // Read the output and count the lines
+      string output = process.StandardOutput.ReadToEnd();
+      unstagedChanges = output.Split(Environment.NewLine).Length - 1;
+      
+      process.Dispose();
+
+      startInfo = new ProcessStartInfo("git");
+
+      startInfo.UseShellExecute = false;
+      startInfo.WorkingDirectory = workDir;
+      startInfo.RedirectStandardInput = true;
+      startInfo.RedirectStandardOutput = true;
+      startInfo.Arguments = "diff --cached --name-status";
+
+      process = new Process();
+      process.StartInfo = startInfo;
+      process.Start();
+
+      // Wait for the process to finish
+      process.WaitForExit();
+
+      // Read the output and count the lines
+      output = process.StandardOutput.ReadToEnd();
+      stagedChanges = output.Split(Environment.NewLine).Length - 1;
+
+      process.Dispose();
     }
 
     if (trueColor) {
@@ -56,7 +114,7 @@ class bunnyPrompt {
     }else {
       ps1 = ps1 + @$"\[\033[34m\]\u\[\033[37m\]@\[\033[34m\]\h ";
     }
-    
+
     string[] splitWorkDir = workDir.Split("/");
     if (splitWorkDir.Length > 2) {
       if (splitWorkDir[1] == "home") {
@@ -72,9 +130,9 @@ class bunnyPrompt {
       }
     }
     if (trueColor) {
-      ps1 = ps1 + @$"{RgbToANSI(166, 218, 149)}\w\r\n";
+      ps1 = ps1 + @$"{RgbToANSI(198, 160, 246)}\w\r\n";
     }else {
-      ps1 = ps1 + @$"\[\033[32m\]\w\r\n";
+      ps1 = ps1 + @$"\[\033[35m\]\w\r\n";
     }
 
     if (displayTable.Contains(Display.GitBranch)) {
@@ -96,10 +154,21 @@ class bunnyPrompt {
       if (trueColor) {
         ps1 = ps1 + RgbToANSI(245, 169, 127);
       }else {
-        ps1 = ps1 + @"\[\x1b[33m\]";
+        ps1 = ps1 + @"\[\033[33m\]";
       }
       if (!(branch == null)) {
         ps1 = ps1 + gitIcon + ": " + gitBranchIcon + branch;
+      }
+    }
+
+    if (unstagedChanges > 0) {
+      ps1 = ps1 + " : " + gitUnstagedIcon + unstagedChanges;
+    }
+    if (stagedChanges > 0) {
+      if (trueColor) {
+        ps1 = ps1 + " : " + RgbToANSI(166, 218, 149) + gitStagedIcon + stagedChanges + RgbToANSI(245, 169, 127);
+      }else {
+        ps1 = ps1 + " : " + @"\[\033[32m\]" + gitStagedIcon + stagedChanges + @"\[\033[33m\]";
       }
     }
 
